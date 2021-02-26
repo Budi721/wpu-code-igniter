5441f0d3e840d855c2744abf1a0b7645e1e2e7e9
Budi721 <rahmawanbd@gmail.com>
2021-02-26 15:33:39 +0700

    Mengimplementasikan view template di header dan footer

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="col">
+        <div class="row">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A blanditiis consectetur cumque debitis doloremque explicabo harum hic iusto numquam officiis, perferendis, quo vitae! Illo in ipsa modi quia recusandae saepe.</p>
+        </div>
+    </div>
+</div>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="col">
+        <div class="row">
+            <h1>Hello, world!</h1>
+            <?php echo d($tes);?>
+        </div>
+    </div>
+</div>
